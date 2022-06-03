14e2824fe61d44f429af8376f22f20b5258182da
hrishi508 <k.hrishi2010@gmail.com>
2022-06-03 11:06:35 +0530

    Update calc code

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res){
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function (req, res){
+    var result = 0;
+
+    switch (req.body.op) {
+        case "+":
+            result = Number(req.body.num1) + Number(req.body.num2);
+            break;
+
+        case "-":
+            result = Number(req.body.num1) - Number(req.body.num2);
+            break;
+
+        case "*":
+            result = Number(req.body.num1) * Number(req.body.num2);
+            break;
+
+        case "/":
+            result = Number(req.body.num1) / Number(req.body.num2);
+            break;
+
+        case "%":
+            result = Number(req.body.num1) % Number(req.body.num2);
+            break;
+
+        case "^":
+            result = Number(req.body.num1) ** Number(req.body.num2);
+            break;
+    
+        default:
+            res.send("Unsupported Operator detected!");
+            break;
+    }
+    
+    res.send("The result of the calculation is " + result);
+})
 
 app.listen(3000, function (){
     console.log("Server started on port 3000");
