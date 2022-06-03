ce0f540290795fd90151fb842d48ace26122f66d
hrishi508 <k.hrishi2010@gmail.com>
2022-06-03 15:20:49 +0530

    Update project code

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,12 +1,29 @@
+var itemList = ["Buy Food", "Cook Food", "Eat Food"];
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/index.html");
+    var date = new Date();
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    date = date.toLocaleDateString('en-US', options);
+    res.render("list", {currDate: date, listOfItems: itemList});
 });
+
+app.post("/", function (req, res){
+    itemList.push(req.body.item);
+    res.redirect("/");
+})
 
 app.listen(3000, function (){
     console.log("Server started on port 3000");
