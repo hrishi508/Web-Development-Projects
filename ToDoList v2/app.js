1932c637d8253cac9b0f25b489728c0320193d4b
hrishi508 <k.hrishi2010@gmail.com>
2022-06-04 00:23:26 +0530

    Prepare project for deployment

--- ToDoList v2/app.js
+++ ToDoList v2/app.js
@@ -7,7 +7,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin-hrishi:test123@cluster0.tb71rz1.mongodb.net/?retryWrites=true&w=majority/todolistDB', {useNewUrlParser: true});
 
 const itemsSchema = new mongoose.Schema({
     name: String
@@ -157,6 +158,11 @@ app.post("/:customListName", function (req, res){
     res.redirect("/" + listName);    
 });
 
-app.listen(3000, function (){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function (){
     console.log("Server started on port 3000");
 });
